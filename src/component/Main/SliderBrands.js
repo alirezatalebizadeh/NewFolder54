cf509a38a6c1873f0caaa3487822ae5ef1373ea4
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-31 22:43:50 +0330

    debug brand slider

--- src/component/Main/SliderBrands.js
+++ src/component/Main/SliderBrands.js
@@ -1,37 +1,42 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react'
 // import required modules
-import { Navigation } from 'swiper/modules';
+import { Navigation } from 'swiper/modules'
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 import './SliderBrands.css'
 
+export default function SliderBrands () {
+  return (
+    <div className='slider_brand'>
+      <Swiper
+        rewind={true}
+        slidesPerView= {6}
 
-
-export default function SliderBrands() {
-
-    return (
-        <div className='slider_brand'>
-            <Swiper
-                rewind={true}
-                slidesPerView={6}
-                navigation={true}
-                modules={[Navigation]}
-                className="mySwiper">
-                <SwiperSlide>Appstore</SwiperSlide>
-                <SwiperSlide>JonInja</SwiperSlide>
-                <SwiperSlide>Jobvision</SwiperSlide>
-                <SwiperSlide>Metavers</SwiperSlide>
-                <SwiperSlide>FaceBook</SwiperSlide>
-                <SwiperSlide>DigiKala</SwiperSlide>
-                <SwiperSlide>Metavers</SwiperSlide>
-                <SwiperSlide>FaceBook</SwiperSlide>
-                <SwiperSlide>DigiKala</SwiperSlide>
-            </Swiper>
-        </div>
-    )
+        navigation={true}
+        // breakpoints={{
+        //   450: {
+        //     slidesPerView: 5,
+        //     spaceBetween: 10
+        //   }
+        // }}
+        modules={[Navigation]}
+        className='mySwiper'
+      >
+        <SwiperSlide>Appstore</SwiperSlide>
+        <SwiperSlide>JonInja</SwiperSlide>
+        <SwiperSlide>Jobvision</SwiperSlide>
+        <SwiperSlide>Metavers</SwiperSlide>
+        <SwiperSlide>FaceBook</SwiperSlide>
+        <SwiperSlide>DigiKala</SwiperSlide>
+        <SwiperSlide>Metavers</SwiperSlide>
+        <SwiperSlide>FaceBook</SwiperSlide>
+        <SwiperSlide>DigiKala</SwiperSlide>
+      </Swiper>
+    </div>
+  )
 }
